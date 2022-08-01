a93b179e6a9e6915b422bb728c873994fd2b1de6
tuantm <tuantu2k2badaoyl@gmail.com>
2022-08-01 17:28:06 +0700

    v2

---
+++ Server/Controllers/ClassController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NET106.Server.Context;
+using NET106.Server.Service;
+using NET106.Shared.Models;
+using NET106.Shared.ViewModels;
+
+namespace NET106.Server.Controllers
+{
+    [ApiController]
+    [Route("api/class")]
+    public class ClassController : ControllerBase
+    {
+        private readonly ISerrvice<ClassViewModel> _serrvice;
+        private readonly DatabaseContext _context;
+
+        public ClassController(ISerrvice<ClassViewModel> serrvice,DatabaseContext context)
+        {
+            _serrvice = serrvice;
+            _context = context;
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> GetClasss()
+        {
+            var model = await _context.Classs.Include(c => c.School).ToListAsync();
+            var result = model.Select(c => new ClassViewModel
+            {
+                id = c.Id,
+                Schoolid = c.SchoolId,
+                Name = c.Name,
+                School = c.School.Name
+            });
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetClass(int id)
+        {
+            var model = await _context.Classs.FindAsync(id);
+            if (model == null) return NotFound();
+            return Ok(new ClassViewModel
+            {
+                id = model.Id,
+                Schoolid = model.SchoolId,
+                Name = model.Name,
+                School = model.School.Name
+            });
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutClass(int id, Class @class)
+        {
+            
+        }
+
+        // POST: api/Class
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Class>> PostClass(ClassViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var major = await _serrvice.Create(new Class()
+            {
+                Id = model.id,
+                Name = model.Name,
+                School = majorViewModel.Status,
+                SchoolId = majorViewModel.SchoolId
+            });
+
+            return CreatedAtAction("GetClasss", new { id = major. }, major);
+        }
+
+        // DELETE: api/Class/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteClass(int id)
+        {
+            if (_context.Classs == null)
+            {
+                return NotFound();
+            }
+            var @class = await _context.Classs.FindAsync(id);
+            if (@class == null)
+            {
+                return NotFound();
+            }
+
+            _context.Classs.Remove(@class);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ClassExists(int id)
+        {
+            return (_context.Classs?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Server/Service/ClassService.cs
@@ -0,0 +1,41 @@
+﻿using NET106.Shared.Models;
+using NET106.Shared.ViewModels;
+
+namespace NET106.Server.Service;
+
+public class ClassService:ISerrvice<ClassViewModel>
+{
+    private readonly HttpClient _httpClient;
+
+    public ClassService(HttpClient client)
+    {
+        _httpClient = client;
+    }
+
+    public async Task<List<ClassViewModel>> Get()
+    {
+        var result = await _httpClient.GetFromJsonAsync<List<ClassViewModel>>("api/");
+        return result;
+    }
+
+    public Task<List<ClassViewModel>> GetId(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> Create(ClassViewModel entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> Update(int id, ClassViewModel entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
+}
